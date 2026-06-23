0631aa185f1bf7a173b905b8579c7d64db8cfd1f
Matheuslealperes <peresmatheus270@gmail.com>
2026-06-23 19:46:39 -0300

    Edita atividade: Quiz de Avaliação da Atividade: Uso de LLMs em Algoritmos e Programação

--- activities.js
+++ activities.js
@@ -60,6 +60,9 @@ const atividades = [
     status: "enviada",
     link: "https://classroom.google.com/c/Nzk2NTIwNDI4Nzg1/a/ODU4NTI5MjQ2OTE1/details",
     descricao: "Quiz avaliativo sobre o uso de LLMs na disciplina.",
+    arquivos: [
+      { nome: "Quiz de Avaliação da Atividade_ Uso de LLMs em Algoritmos e Programação.pdf", url: "https://raw.githubusercontent.com/exoticos55/exoticos55.github.io/main/arquivos/1782254797581-Quiz-de-Avaliacao-da-Atividade_-Uso-de-LLMs-em-Algoritmos-e-Programacao.pdf" },
+    ],
   },
   {
     titulo: "Resolver os 5 exercícios em papel",
